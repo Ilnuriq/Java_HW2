0f908aa7f890e9cc0010dc63f97e6620b0bfd7b0
Ilnur Garrapov <garrapovilnur@gmail.com>
2023-04-18 19:52:53 +0300

    task1

--- Task_0.java
+++ Task_0.java
@@ -1,6 +1,6 @@
 package For_java_HW;
 import java.util.Scanner;
-public class Task_2_0 {
+public class Task_0 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Введите названия драгоценных камней =  ");
